7793076425e202ac5824fc92e078d6d467d09a20
Paul <115771869+Polypheus@users.noreply.github.com>
2024-07-03 01:32:46 +0000

    design

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-//import libraries in app.js which is the server
 const express = require('express');
 const path = require('path');
-//create express app
-const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const mainController = require('./controllers/mainController');
 
+const app = express();
 const PORT = 3000;
-// const MONGODB_URI = 'your-mongodb-uri';
 
-//connect to mongodb
+// Connect to MongoDB (commented out as it is optional)
+// const MONGODB_URI = 'your-mongodb-uri';
 // mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 //   .then(() => console.log('Connected to MongoDB'))
 //   .catch(err => console.error(err));
 
-//set middleware
+// Set middleware
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public')); // Serve static files from "public"
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from "public"
+
+// Set view engine
+app.set('view engine', 'ejs'); // Use EJS for rendering views
 
 // Route to render the HTML page with video
 app.get('/', (req, res) => {
@@ -26,11 +27,8 @@ app.get('/', (req, res) => {
     });
 });
 
-//set view engine
-app.set('view engine', 'ejs'); // Use EJS for rendering views
-
-//use controller
+// Use controller
 app.use('/', mainController); // Use routes defined in mainController
 
-//start server
+// Start server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
